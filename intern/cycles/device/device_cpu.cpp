55d42b9b3ceb31f2fa7e7789ef73b6ba24a8f29b
Doug Gale <doug65536@gmail.com>
2013-06-25 20:25:26 -0400

    Refactor optimized kernel implementation to use templates instead of copy/paste repetition

--- intern/cycles/device/device_cpu.cpp
+++ intern/cycles/device/device_cpu.cpp
@@ -127,25 +127,14 @@ public:
 #endif
 	}
 
-	void thread_run(DeviceTask *task)
-	{
-		if(task->type == DeviceTask::PATH_TRACE)
-			thread_path_trace(*task);
-		else if(task->type == DeviceTask::TONEMAP)
-			thread_tonemap(*task);
-		else if(task->type == DeviceTask::SHADER)
-			thread_shader(*task);
-	}
+	/* function pointer for optimized implementations */
+	typedef void (*path_trace_impl_t)(KernelGlobals *kg, float *buffer, unsigned int *rng_state, int sample, int x, int y, int offset, int stride);
+	typedef void (*tonemap_impl_t)(KernelGlobals *kg, uchar4 *rgba, float *buffer, int sample, int x, int y, int offset, int stride);
+	typedef void (*shader_impl_t)(KernelGlobals *kg, uint4 *input, float4 *output, int type, int i);
 
-	class CPUDeviceTask : public DeviceTask {
-	public:
-		CPUDeviceTask(CPUDevice *device, DeviceTask& task)
-		: DeviceTask(task)
-		{
-			run = function_bind(&CPUDevice::thread_run, device, this);
-		}
-	};
-
+	/* specializations of this function call the corresponding optimized
+	 * implementation directly with no indirection */
+	template<path_trace_impl_t path_trace_impl>
 	void thread_path_trace(DeviceTask& task)
 	{
 		if(task_pool.cancelled()) {
@@ -160,22 +149,6 @@ public:
 #endif
 
 		RenderTile tile;
-
-		typedef void (*kernel_impl_t)(KernelGlobals *kg, float *buffer, unsigned int *rng_state, int sample, int x, int y, int offset, int stride);
-
-		kernel_impl_t path_trace_impl = kernel_cpu_path_trace;
-
-#ifdef WITH_OPTIMIZED_KERNEL
-		if(system_cpu_support_avx1()) {
-			path_trace_impl = kernel_cpu_avx1_path_trace;
-		} else if(system_cpu_support_sse4()) {
-			path_trace_impl = kernel_cpu_sse4_path_trace;
-		} else if(system_cpu_support_sse3()) {
-			path_trace_impl = kernel_cpu_sse3_path_trace;
-		} else if(system_cpu_support_sse2()) {
-			path_trace_impl = kernel_cpu_sse2_path_trace;
-		}
-#endif
 
 		while(task.acquire_tile(this, tile)) {
 			float *render_buffer = (float*)tile.buffer;
@@ -214,43 +187,16 @@ public:
 #endif
 	}
 
+	template<tonemap_impl_t tonemap_impl>
 	void thread_tonemap(DeviceTask& task)
 	{
-#ifdef WITH_OPTIMIZED_KERNEL
-		if(system_cpu_support_avx1()) {
-			for(int y = task.y; y < task.y + task.h; y++)
-				for(int x = task.x; x < task.x + task.w; x++)
-					kernel_cpu_avx1_tonemap(&kernel_globals, (uchar4*)task.rgba, (float*)task.buffer,
-						task.sample, x, y, task.offset, task.stride);
-		}
-		if(system_cpu_support_sse4()) {
-			for(int y = task.y; y < task.y + task.h; y++)
-				for(int x = task.x; x < task.x + task.w; x++)
-					kernel_cpu_sse4_tonemap(&kernel_globals, (uchar4*)task.rgba, (float*)task.buffer,
-						task.sample, x, y, task.offset, task.stride);
-		}
-		if(system_cpu_support_sse3()) {
-			for(int y = task.y; y < task.y + task.h; y++)
-				for(int x = task.x; x < task.x + task.w; x++)
-					kernel_cpu_sse3_tonemap(&kernel_globals, (uchar4*)task.rgba, (float*)task.buffer,
-						task.sample, x, y, task.offset, task.stride);
-		}
-		else if(system_cpu_support_sse2()) {
-			for(int y = task.y; y < task.y + task.h; y++)
-				for(int x = task.x; x < task.x + task.w; x++)
-					kernel_cpu_sse2_tonemap(&kernel_globals, (uchar4*)task.rgba, (float*)task.buffer,
-						task.sample, x, y, task.offset, task.stride);
-		}
-		else
-#endif
-		{
-			for(int y = task.y; y < task.y + task.h; y++)
-				for(int x = task.x; x < task.x + task.w; x++)
-					kernel_cpu_tonemap(&kernel_globals, (uchar4*)task.rgba, (float*)task.buffer,
-						task.sample, x, y, task.offset, task.stride);
-		}
+		for(int y = task.y; y < task.y + task.h; y++)
+			for(int x = task.x; x < task.x + task.w; x++)
+				tonemap_impl(&kernel_globals, (uchar4*)task.rgba, (float*)task.buffer,
+					task.sample, x, y, task.offset, task.stride);
 	}
 
+	template<shader_impl_t shader_impl>
 	void thread_shader(DeviceTask& task)
 	{
 		KernelGlobals kg = kernel_globals;
@@ -259,38 +205,98 @@ public:
 		OSLShader::thread_init(&kg, &kernel_globals, &osl_globals);
 #endif
 
-#ifdef WITH_OPTIMIZED_KERNEL
-		if(system_cpu_support_sse3()) {
-			for(int x = task.shader_x; x < task.shader_x + task.shader_w; x++) {
-				kernel_cpu_sse3_shader(&kg, (uint4*)task.shader_input, (float4*)task.shader_output, task.shader_eval_type, x);
+		for(int x = task.shader_x; x < task.shader_x + task.shader_w; x++) {
+			shader_impl(&kg, (uint4*)task.shader_input, (float4*)task.shader_output, task.shader_eval_type, x);
 
-				if(task_pool.cancelled())
-					break;
-			}
-		}
-		else if(system_cpu_support_sse2()) {
-			for(int x = task.shader_x; x < task.shader_x + task.shader_w; x++) {
-				kernel_cpu_sse2_shader(&kg, (uint4*)task.shader_input, (float4*)task.shader_output, task.shader_eval_type, x);
-
-				if(task_pool.cancelled())
-					break;
-			}
-		}
-		else
-#endif
-		{
-			for(int x = task.shader_x; x < task.shader_x + task.shader_w; x++) {
-				kernel_cpu_shader(&kg, (uint4*)task.shader_input, (float4*)task.shader_output, task.shader_eval_type, x);
-
-				if(task_pool.cancelled())
-					break;
-			}
+			if(task_pool.cancelled())
+				break;
 		}
 
 #ifdef WITH_OSL
 		OSLShader::thread_free(&kg);
 #endif
 	}
+
+	void thread_run(DeviceTask *task)
+	{
+		unsigned old_csr = 0;
+		if (system_cpu_support_sse2()) {
+			/* configure SSE to flush denormals to zero and flush
+			 * denormal results to zero. Denormals are VERY expensive,
+			 * they cause hundreds of cycles of microcode "assists" */
+			old_csr = _mm_getcsr();
+			/* bit 15 is FTZ, bit 6 is DAZ */
+			_mm_setcsr(old_csr | (1 << 15) | (1 << 6));
+		}
+
+		if(task->type == DeviceTask::PATH_TRACE) {
+#ifdef WITH_OPTIMIZED_KERNEL
+			if(system_cpu_support_avx1()) {
+				thread_path_trace<kernel_cpu_avx1_path_trace>(*task);
+			} else if(system_cpu_support_sse4()) {
+				thread_path_trace<kernel_cpu_sse4_path_trace>(*task);
+			} else if(system_cpu_support_sse3()) {
+				thread_path_trace<kernel_cpu_sse3_path_trace>(*task);
+			} else if(system_cpu_support_sse2()) {
+				thread_path_trace<kernel_cpu_sse2_path_trace>(*task);
+			} else {
+				thread_path_trace<kernel_cpu_path_trace>(*task);
+			}
+#else
+			thread_path_trace<kernel_cpu_path_trace>(*task);
+#endif
+		} else if(task->type == DeviceTask::TONEMAP) {
+#ifdef WITH_OPTIMIZED_KERNEL
+			if(system_cpu_support_avx1()) {
+				thread_tonemap<kernel_cpu_avx1_tonemap>(*task);
+			} else if(system_cpu_support_sse4()) {
+				thread_tonemap<kernel_cpu_sse4_tonemap>(*task);
+			} else if(system_cpu_support_sse3()) {
+				thread_tonemap<kernel_cpu_sse3_tonemap>(*task);
+			} else if(system_cpu_support_sse2()) {
+				thread_tonemap<kernel_cpu_sse2_tonemap>(*task);
+			} else {
+				thread_tonemap<kernel_cpu_tonemap>(*task);
+			}
+#else
+			thread_tonemap<kernel_cpu_tonemap>(*task);
+#endif
+		} else if(task->type == DeviceTask::SHADER) {
+#ifdef WITH_OPTIMIZED_KERNEL
+			/* configure SSE to flush denormals to zero and flush
+			 * denormal results to zero */
+
+			if(system_cpu_support_avx1()) {
+				thread_shader<kernel_cpu_avx1_shader>(*task);
+			} else if(system_cpu_support_sse4()) {
+				thread_shader<kernel_cpu_sse4_shader>(*task);
+			} else if(system_cpu_support_sse3()) {
+				thread_shader<kernel_cpu_sse3_shader>(*task);
+			} else if(system_cpu_support_sse2()) {
+				thread_shader<kernel_cpu_sse2_shader>(*task);
+			} else {
+				thread_shader<kernel_cpu_shader>(*task);
+			}
+#else
+			thread_shader<kernel_cpu_shader>(*task);
+#endif
+		}
+
+		/* restore FPU config */
+		if (system_cpu_support_sse2()) {
+			_mm_setcsr(old_csr);
+		}
+
+	}
+
+	class CPUDeviceTask : public DeviceTask {
+	public:
+		CPUDeviceTask(CPUDevice *device, DeviceTask& task)
+		: DeviceTask(task)
+		{
+			run = function_bind(&CPUDevice::thread_run, device, this);
+		}
+	};
 
 	void task_add(DeviceTask& task)
 	{
