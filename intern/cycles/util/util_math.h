9a5043484fe48e3e40afc9d26fed71686a78847e
Doug Gale <doug65536@gmail.com>
2013-07-10 17:44:26 -0400

    Updated to svn revision 58160

--- intern/cycles/util/util_math.h
+++ intern/cycles/util/util_math.h
@@ -3542,6 +3542,42 @@ __device_inline float3 safe_divide_color(float3 a, float3 b)
 	return mask_select(b != zerof, a * inverseb, zerof);
 }
 
+__device_inline float3 safe_divide_even_color(float3 a, float3 b)
+{
+	float x, y, z;
+
+	x = (b.x != 0.0f)? a.x/b.x: 0.0f;
+	y = (b.y != 0.0f)? a.y/b.y: 0.0f;
+	z = (b.z != 0.0f)? a.z/b.z: 0.0f;
+
+	/* try to get grey even if b is zero */
+	if(b.x == 0.0f) {
+		if(b.y == 0.0f) {
+			x = z;
+			y = z;
+		}
+		else if(b.z == 0.0f) {
+			x = y;
+			z = y;
+		}
+		else
+			x = 0.5f*(y + z);
+	}
+	else if(b.y == 0.0f) {
+		if(b.z == 0.0f) {
+			y = x;
+			z = x;
+		}
+		else
+			y = 0.5f*(x + z);
+	}
+	else if(b.z == 0.0f) {
+		z = 0.5f*(x + y);
+	}
+
+	return make_float3(x, y, z);
+}
+
 /* Rotation of point around axis and angle */
 
 __device_inline float3 rotate_around_axis(const float3 p, const float3 axis, float angle)
